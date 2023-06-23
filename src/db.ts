4e3c12806feb9759d20b1ac85d4e9d1b0aef9d02
Nacho731 <elnachowe3@gmail.com>
2023-06-22 23:19:27 -0300

    first commit3

--- src/db.ts
+++ src/db.ts
@@ -4,23 +4,23 @@ config;
 
  ////////////////// *LOCAL* /////////////////////////
 
-export const sequelize = new Sequelize({
-  dialect: "postgres",
-  database: config.dbName,
-  password: config.dbPassword,
-  username: config.dbUser,
-  storage: ":memory:",
-  models: [__dirname + "/models"],
-});
+// export const sequelize = new Sequelize({
+//   dialect: "postgres",
+//   database: config.dbName,
+//   password: config.dbPassword,
+//   username: config.dbUser,
+//   storage: ":memory:",
+//   models: [__dirname + "/models"],
+// });
 
 //////////////// *DEPLOY* ///////////////////////
 
-// export const sequelize = new Sequelize({
-//   dialect: "postgres",
-//   database: "railway",
-//   password: "C65Qhq92tHC7iDdKnibe",
-//   username: "postgres",
-//   host: "containers-us-west-159.railway.app",
-//   port: 7322,
-//   models: [__dirname + "/models"],
-// });
+export const sequelize = new Sequelize({
+  dialect: "postgres",
+  database: "walletwise_qynk",
+  password: "xLSm2eLJ8NqxgLYw3tqsuKKtIEWGHQ9N",
+  username: "walletwise_qynk_user",
+  host: "dpg-ciafv459aq007tesvg30-a",
+  port: 5432,
+  models: [__dirname + "/models"],
+});
